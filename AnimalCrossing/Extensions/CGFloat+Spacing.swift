8aba9ea010d4660d7c711ebd6a86e512f74545a1
acicartagena <acicartagena@gmail.com>
2020-07-26 19:44:57 +1000

    moving stuff around
    # Conflicts:
    #	AnimalCrossing.xcodeproj/project.pbxproj

---
+++ AnimalCrossing/Extensions/CGFloat+Spacing.swift
@@ -0,0 +1,11 @@
+// Copyright © 2020 ACartagena. All rights reserved.
+
+import UIKit
+
+extension CGFloat {
+    static let margin: CGFloat = 16.0
+    static let inverseMargin: CGFloat = -1 * margin
+
+    static let interItem: CGFloat = 8.0
+    static let inverseInterItem: CGFloat = -1 * interItem
+}
--- AnimalCrossing/Villagers/VillagerCell.swift
+++ AnimalCrossing/Villagers/VillagerCell.swift
@@ -2,14 +2,6 @@
 
 import UIKit
 
-extension CGFloat {
-    static let margin: CGFloat = 16.0
-    static let inverseMargin: CGFloat = -1 * margin
-
-    static let interItem: CGFloat = 8.0
-    static let inverseInterItem: CGFloat = -1 * interItem
-}
-
 class VillagerCell: UICollectionViewCell {
 
     private let view: UIView = {
